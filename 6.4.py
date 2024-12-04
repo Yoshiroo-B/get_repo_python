1d4edd40241d33eb9a51e423ce02a52014eaba10
d.fedotov1 <fedotovdaniil2017@gmail.com>
2024-12-04 09:54:56 +0300

    task 4

---
+++ 6.4.py
@@ -0,0 +1,13 @@
+def check_for_auto_number(string: str):
+    if len(string) == 6 and string.isalnum():
+        if len(re.findall('[a-zA-Z]{2}[0-9]{3}[a-zA-Z]{1}', string)) == 1:
+            return True
+    return False
+
+
+input_str = input("Введите предположительный номер автомобиля: ")
+
+if check_for_auto_number(input_str):
+    print("Может являться номером автомобиля")
+else:
+    print("Не может являться номером автомобиля")
