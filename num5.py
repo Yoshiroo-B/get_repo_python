22855c7d0dd7583f50c54c927fa3c182c03e0a1a
d.fedotov1 <fedotovdaniil2017@gmail.com>
2024-12-04 10:53:10 +0300

    num5 added

--- num5.py
+++ num5.py
@@ -17,12 +17,13 @@ def player_step(input_data):
 
 
 def koordinal_step(number_stones):
+    if number_stones <= 4:
+        return 1, abs(1 - number_stones)
     koordinal_step = random.randint(1, 3)
     return number_stones - koordinal_step, koordinal_step
 
 
-number_stones = random.randint(4, 30)
-win = "logout"
+
 
 while win == "logout":
     print(f"\nТекущее значение камней в куче: {number_stones}\n")
@@ -55,4 +56,5 @@ while win == "logout":
                 print("You are dead. Game over!")
                 win = "koordinal_win"
 
-
+    else:
+        print("Введено неверное значение. Введите возможное количество.")
